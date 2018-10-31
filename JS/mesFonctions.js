47e52c7e5b4f72f17647a027f2d493b4550d5975
NicolasCoudert <ncoudert12@gmail.com>
2018-10-31 03:12:00 +0100

    PRET

--- JS/mesFonctions.js
+++ JS/mesFonctions.js
@@ -251,28 +251,6 @@ function Ajoutinscription()
             error:function()
             {
                 alert("L'un des champs n'as pas été rempli.");
-            }
-            
-        }
-    );
-}
-
-function Ajoutinscription()
-{
-    $.ajax
-    (
-        {
-            type:"get",
-            url:"index.php/Index_Inscription/Ajoutinscription",
-            data:"nomUser="+$('#txtNom').val()+"&login="+$('#txtLogin').val()+"&mdp="+$('#txtMdp').val(),
-            success:function(data)
-            {
-                alert("L'inscription a été prise en compte.");
-                AjoutInscription();
-            },
-            error:function()
-            {
-                alert("L'un des champs n'as pas été rempli.");
             },
             
         }
