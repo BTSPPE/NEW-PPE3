e42bbca7c633553bccd700b7ec37b6e3b63778ea
NicolasCoudert <ncoudert12@gmail.com>
2018-10-31 01:45:11 +0100

    Ajout de ;

--- application/views/View_inscription.php
+++ application/views/View_inscription.php
@@ -6,7 +6,7 @@
     <title>Inscription</title>
     <link rel="stylesheet" href="Bootstrap/css/bootstrap.css" />
     <script type="text/javascript" src="JQuery/jquery-3.1.1.js"></script>
-    <script type="text/javascript" src="JS/MesFonctions.js"></script>
+        <script type="text/javascript" src="JS/MesFonctions.js"></script>
 </head>
 <body>
     <h1>Inscription</h1><br>
@@ -14,7 +14,7 @@
     echo "<label>Nom</label> <input type='text' id='txtNom'><br><br>";
     echo "<label>Login</label> <input type='text' id='txtLogin'><br><br>";
     echo "<label>Mot de passe</label> <input type='password' id='txtMdp'><br><br>";
-    echo "<input type='button' value='Créer un compte' onclick='Ajoutinscription()'>"
+    echo "<input type='button' value='Créer un compte' onclick='Ajoutinscription()'>";
     ?>
     </body>
 </html>
